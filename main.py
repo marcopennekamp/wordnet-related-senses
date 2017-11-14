d6dee88c454281caa0d2665a96c4419bb392f3c2
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-11-14 01:04:50 +0100

    Add affinity propagation clustering.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import itertools
+import numpy as np
+import sklearn.cluster
 from nltk.corpus import wordnet as wn
 
 
@@ -16,6 +18,7 @@ def derivational_similarity(lemma1, lemma2):
     :return: The similarity of lemma1 and lemma2, based on the similarity of the best derivationally related form.
              If there is no such derivationally related form that leads to a valid similarity value, None is returned.
     """
+
     def get_candidates(related_lemma, lemma):
         return list(itertools.product(derivationally_related_synsets(related_lemma), [lemma.synset()]))
 
@@ -37,18 +40,29 @@ def get_lemma_for_word(synset, word):
 
 def main():
     word = 'cannon'
-    synsets = wn.synsets(word)
-    print(synsets)
+    synsets = np.asarray(wn.synsets(word))
 
-    pairs = itertools.combinations(synsets, 2)
-    for (synset1, synset2) in pairs:
+    # First build the distance matrix by computing the similarity of the synsets.
+    def similarity(synset1, synset2):
         # If the POS of the synsets are equal, we can apply the Wu-Palmer similarity measure.
         # Otherwise, applying Wu-Palmer may give us a None result. In such a case, we fallback
         # to the derivational_similarity measure defined above.
-        similarity = synset1.wup_similarity(synset2)
-        if similarity is None:
-            similarity = derivational_similarity(get_lemma_for_word(synset1, word), get_lemma_for_word(synset2, word))
-        print(f'{synset1.definition()} | {synset2.definition()}:\t{similarity}')
+        sim = synset1.wup_similarity(synset2)
+        if sim is None:
+            sim = derivational_similarity(get_lemma_for_word(synset1, word), get_lemma_for_word(synset2, word))
+        # print(f'{synset1.definition()} | {synset2.definition()}:\t{sim}')
+        return sim
+
+    similarityMatrix = np.array([[similarity(s1, s2) for s1 in synsets] for s2 in synsets])
+
+    # Cluster with affinity propagation (doesn't require the number of clusters).
+    aff = sklearn.cluster.AffinityPropagation(affinity="precomputed", damping=0.5)
+    aff.fit(similarityMatrix)
+    print(f'Clusters for \'{word}\':')
+    for cluster_id in np.unique(aff.labels_):
+        exemplar = synsets[aff.cluster_centers_indices_[cluster_id]]
+        cluster = np.unique(synsets[np.nonzero(aff.labels_ == cluster_id)])
+        print(f' - {exemplar.name()}: {cluster}')
 
 
 # Call the main function defined above to execute the program.
