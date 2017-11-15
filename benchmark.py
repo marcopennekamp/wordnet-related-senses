dfc29a87419dedca71b5928b78d0841e8acba2bf
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-11-15 20:08:47 +0100

    Weigh each synset equally. Calculate total accuracy.

--- benchmark.py
+++ benchmark.py
@@ -32,7 +32,7 @@ ideals = [
 ]
 
 
-def measure_clustering_quality(ideal, clusters):
+def measure_clustering_accuracy(ideal, clusters):
     """
 
     :return: A score from 1.0 (perfect match) to 0.0 (nothing fits). However, 0.0 is theoretically not possible,
@@ -40,41 +40,63 @@ def measure_clustering_quality(ideal, clusters):
              as a counterpart.
     """
     absolute_score = 0.0
+    synset_count = 0
     for ideal_cluster in ideal.expected:
         # Implements the Jaccard similarity measure.
         def measure_cluster_similarity(cluster):
             return len(cluster & ideal_cluster) / len(cluster | ideal_cluster)
 
         similarities = list(map(measure_cluster_similarity, clusters))
-        # TODO: This gives the same weight to all clusters. We should weigh each cluster by its size.
-        absolute_score += max(similarities)
-    return absolute_score / len(ideal.expected)
+
+        # To give each sense the same weight, instead of each cluster, we multiply the score by the number of synsets
+        # in the ideal cluster. At the end, we also divide by the number of unique synsets in all ideal clusters.
+        current_synset_count = len(ideal_cluster)
+        synset_count += current_synset_count
+        absolute_score += max(similarities) * current_synset_count
+    return absolute_score / synset_count
 
 
-def benchmark_ideal(ideal):
+def print_benchmark_ideal_results(ideal, clusters, accuracy, verbose):
+    word = ideal.word
+
+    def print_cluster(c):
+        print(f' - {next(iter(c)).name()}: {c}')
+
+    if verbose:
+        print(f'Expected clusters for \'{word}\':')
+        for cluster in ideal.expected:
+            print_cluster(cluster)
+
+        print(f'Clusters for \'{word}\':')
+        for cluster in clusters:
+            print_cluster(cluster)
+
+    print(f'Clustering accuracy for \'{word}\': {measure_clustering_accuracy(ideal, clusters)}')
+
+
+def benchmark_ideal(ideal, verbose):
+    """
+    :return: The accuracy of the clustering for the given word.
+    """
     word = ideal.word
     synsets = np.asarray(wn.synsets(word))
 
     similarity_matrix = relatedness.compute_metonym_similarity_matrix(word, synsets)
     clusters = clustering.cluster_affinity(word, synsets, similarity_matrix)
+    accuracy = measure_clustering_accuracy(ideal, clusters)
 
-    def print_cluster(c):
-        print(f' - {next(iter(c)).name()}: {c}')
+    print_benchmark_ideal_results(ideal, clusters, accuracy, verbose)
 
-    print(f'Expected clusters for \'{word}\':')
-    for cluster in ideal.expected:
-        print_cluster(cluster)
-
-    print(f'Clusters for \'{word}\':')
-    for cluster in clusters:
-        print_cluster(cluster)
-
-    print(f'Clustering quality: {measure_clustering_quality(ideal, clusters)}')
+    return accuracy
 
 
 def benchmark():
+    verbose = True
+    absolute_accuracy = 0
     for word_result in ideals:
-        benchmark_ideal(word_result)
+        absolute_accuracy += benchmark_ideal(word_result, verbose)
+    total_accuracy = absolute_accuracy / len(ideals)
+    print(f'The total clustering accuracy is: {total_accuracy}')
 
 
 benchmark()
