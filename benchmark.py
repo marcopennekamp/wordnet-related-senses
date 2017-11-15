c1bb2ca9e69eeae8fef3ecae22f71adb3179961e
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-11-15 17:31:45 +0100

    Add measurement for cluster quality.

--- benchmark.py
+++ benchmark.py
@@ -32,6 +32,25 @@ ideals = [
 ]
 
 
+def measure_clustering_quality(ideal, clusters):
+    """
+
+    :return: A score from 1.0 (perfect match) to 0.0 (nothing fits). However, 0.0 is theoretically not possible,
+             because we don't label the clusters, and thus at least one ideal cluster will find a single element
+             as a counterpart.
+    """
+    absolute_score = 0.0
+    for ideal_cluster in ideal.expected:
+        # Implements the Jaccard similarity measure.
+        def measure_cluster_similarity(cluster):
+            return len(cluster & ideal_cluster) / len(cluster | ideal_cluster)
+
+        similarities = list(map(measure_cluster_similarity, clusters))
+        # TODO: This gives the same weight to all clusters. We should weigh each cluster by its size.
+        absolute_score += max(similarities)
+    return absolute_score / len(ideal.expected)
+
+
 def benchmark_ideal(ideal):
     word = ideal.word
     synsets = np.asarray(wn.synsets(word))
@@ -49,6 +68,8 @@ def benchmark_ideal(ideal):
     print(f'Clusters for \'{word}\':')
     for cluster in clusters:
         print_cluster(cluster)
+
+    print(f'Clustering quality: {measure_clustering_quality(ideal, clusters)}')
 
 
 def benchmark():
