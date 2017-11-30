0cb83fc239b02bc4e4ccc9672ecaf1318f919567
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-11-30 20:09:10 +0100

    Add distance script for examining paths.

---
+++ distance.py
@@ -0,0 +1,11 @@
+from sys import argv
+from nltk.corpus import wordnet as wn
+
+import relatedness
+
+_, graph_name, synset1_name, synset2_name = argv
+synset1 = wn.synset(synset1_name)
+synset2 = wn.synset(synset2_name)
+wordnet_graph = relatedness.load_wordnet_graph(graph_name)
+print(wordnet_graph.shortest_path(synset1, synset2))
+print(wordnet_graph.shortest_path_distance(synset1, synset2))
