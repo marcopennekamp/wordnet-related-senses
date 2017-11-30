95912eafd1002042ebc181925efdd4c4614f0a09
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-11-30 20:14:59 +0100

    Rename distance script to path.

--- path.py
+++ path.py
@@ -7,5 +7,5 @@ _, graph_name, synset1_name, synset2_name = argv
 synset1 = wn.synset(synset1_name)
 synset2 = wn.synset(synset2_name)
 wordnet_graph = relatedness.load_wordnet_graph(graph_name)
-print(wordnet_graph.shortest_path(synset1, synset2))
-print(wordnet_graph.shortest_path_distance(synset1, synset2))
+print(f'path: {wordnet_graph.shortest_path(synset1, synset2)}')
+print(f'length: {wordnet_graph.shortest_path_distance(synset1, synset2)}')
